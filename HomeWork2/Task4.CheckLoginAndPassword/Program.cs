837ab3c3c42d880599b92c52252ef23a0a5ff87e
Andrey Kasimovskiy <kasimovskiy.a.d.public@gmail.com>
2020-11-19 21:14:29 +0300

    Add task 4 check login and password

---
+++ HomeWork2/Task4.CheckLoginAndPassword/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4.CheckLoginAndPassword
+{
+	class Program
+	{
+		/*
+		 
+		 * Реализовать метод проверки логина и пароля. 
+		 * На вход подается логин и пароль. 
+		 * На выходе истина, если прошел авторизацию, и ложь, если не прошел (Логин: root, Password: GeekBrains). 
+		 * Используя метод проверки логина и пароля, написать программу: пользователь вводит логин и пароль,
+		 * программа пропускает его дальше или не пропускает. 
+		 * С помощью цикла do while ограничить ввод пароля тремя попытками.
+		 
+		 * Выполнил: Касимовский Андрей
+		 
+		*/
+		static void Main(string[] args)
+		{
+			int tryCount = 0;
+			bool check = false;
+
+			do
+			{
+				check = CheckLoginAndPassword();
+
+				if (check)
+				{
+					Console.WriteLine("Correctly");
+					break;
+				}
+				else
+					Console.WriteLine("Wrong login or password");
+				tryCount++;
+			} while (tryCount < 3);
+
+			Console.WriteLine(check ? "Welcome! You Entered!" : "You've used every attempt");
+
+			Console.ReadKey();
+		}
+
+		static bool CheckLoginAndPassword()
+		{
+			string l = "root";
+			string p = "GeekBrains";
+
+			Console.WriteLine("Enter login: ");
+			string login = Console.ReadLine();
+
+			Console.WriteLine("Enter password: ");
+			string password = Console.ReadLine();
+
+			if (login.Equals(l) && password.Equals(p)) return true;
+			else return false;
+		}
+	}
+}
