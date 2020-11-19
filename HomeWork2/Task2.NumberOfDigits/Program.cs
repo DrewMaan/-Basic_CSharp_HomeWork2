c5b3e1c4efea8f3acae5a48f211ba1a84a0f14c1
Andrey Kasimovskiy <kasimovskiy.a.d.public@gmail.com>
2020-11-19 20:48:27 +0300

    Add 3 tasks

---
+++ HomeWork2/Task2.NumberOfDigits/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.NumberOfDigits
+{
+	class Program
+	{
+		/*
+		 
+		 * Написать метод подсчета количества цифр числа.
+		 * 
+		 * Выполнил: Касимовский Андрей
+		 
+		*/
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter long number: ");
+			long number = long.Parse(Console.ReadLine());
+
+			NumberOfDigits(number);
+
+			Console.ReadKey();
+		}
+
+		static void NumberOfDigits(long num)
+		{
+			int count = 0;
+
+			do
+			{
+				num /= 10;
+				count++;
+			} while (num != 0);
+
+			Console.WriteLine($"Numbers of digits your number: {count}");
+		}
+	}
+}
---
+++ HomeWork2/Task3.SumOfOddPositiveNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3.SumOfOddPositiveNumbers
+{
+	class Program
+	{
+		/*
+		 
+		 * С клавиатуры вводятся числа, пока не будет введен 0. 
+		 * Подсчитать сумму всех нечетных положительных чисел.
+		 * 
+		 * Выполнил: Касимовский Андрей
+		 
+		*/
+		static void Main(string[] args)
+		{
+			int number;
+			int sum = 0;
+
+			Console.WriteLine("Enter number (enter 0 for quit): ");
+			number = int.Parse(Console.ReadLine());
+
+			while(number != 0)
+			{
+				if(number % 2 != 0 && number > 0) sum += number;
+
+				Console.WriteLine("Enter number (enter 0 for quit): ");
+				number = int.Parse(Console.ReadLine());
+			}
+
+			Console.WriteLine($"Sum of odd positive numbers: {sum}");
+
+			Console.ReadKey();
+		}
+	}
+}
