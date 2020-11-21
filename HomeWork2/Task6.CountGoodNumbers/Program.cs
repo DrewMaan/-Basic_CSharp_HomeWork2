2cae47576cae9b3a18b0f9a0cf5fda1d41a40a33
Andrey Kasimovskiy <kasimovskiy.a.d.public@gmail.com>
2020-11-21 15:43:06 +0300

    Add task 6 count good numbers

---
+++ HomeWork2/Task6.CountGoodNumbers/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6.CountGoodNumbers
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int startTime = DateTime.Now.Hour * 3600 + DateTime.Now.Minute * 60 + DateTime.Now.Second;
+
+			var countsGoodNumbers = CalculateCountGoodNumbers(1, 1000000000);
+			int endTime = DateTime.Now.Hour * 3600 + DateTime.Now.Minute * 60 + DateTime.Now.Second;
+			int time = endTime - startTime;
+
+			Console.WriteLine("Count good numbers from {0} to {1}: {2}.", 1, 1000000000, countsGoodNumbers);
+			Console.WriteLine($"Time execute program: {time / 3600} hours, " +
+				$"{(time % 3600)/60} minute, {(time % 3600) % 60} second.");
+
+			Console.ReadKey();
+		}
+
+		static long CalculateCountGoodNumbers(long startNumber, long endNumber)
+		{
+			var count = 0;
+			for(long i = startNumber; i <= endNumber; i++)
+			{
+				var sumDigits = 0;
+				var number = i;
+				while (number != 0)
+				{
+					sumDigits += (int)(number % 10);
+					number /= 10;
+				}
+				count += (i % sumDigits) == 0 ? 1 : 0;
+			}
+
+			return count;
+		}
+	}
+}
